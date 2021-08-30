e2c2d5b8247c3ba652c414bdbf27fcc52eda9edc
Andrew White <white.d.andrew@gmail.com>
2021-08-30 00:57:56 -0400

    Removed unneeded file

--- bib/make_bib.sh
+++ bib/make_bib.sh
@@ -3,6 +3,4 @@
 echo "Downloadign google scholar"
 wget -O scholar.bib "https://scholar.googleusercontent.com/citations?view_op=export_citations&user=47fQG30AAAAJ&citsig=AMD79ooAAAAAYS2fro7YV8HK-e5P9RWy6hyYsGzyznTV&hl=en"
 echo "Covnerting"
-pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o biblio.txt
-
-
+pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o biblio.txt && rm scholar.bib
