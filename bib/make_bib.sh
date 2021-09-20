c4a2c30315580107435e60d8ad489e73b10e11d9
Andrew White <white.d.andrew@gmail.com>
2021-09-20 17:00:01 -0400

    Added new papers

--- bib/make_bib.sh
+++ bib/make_bib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Downloadign google scholar"
-wget -O scholar.bib "https://scholar.googleusercontent.com/citations?view_op=export_citations&user=47fQG30AAAAJ&citsig=AMD79ooAAAAAYTTOYTsCcOojMgtx4QESn9zobqW2bj7A&hl=en"
+wget -O scholar.bib "https://scholar.googleusercontent.com/citations?view_op=export_citations&user=47fQG30AAAAJ&citsig=AMD79ooAAAAAYUpGgd1XpWJJWkWNlR3cW-fsE_k32WUF&hl=en"
 echo "Covnerting"
 pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o biblio.txt && rm scholar.bib
