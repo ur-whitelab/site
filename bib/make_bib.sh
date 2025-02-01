42ed97dd86ea44c9ddb194c590bf29a8db344ccd
shane <smichtav@che.rochester.edu>
2025-01-31 22:21:26 -0500

    updated .sh file and group papers

--- bib/make_bib.sh
+++ bib/make_bib.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Downloadign google scholar"
-wget -O scholar.bib $1
-echo "Covnerting"
-pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o biblio.txt && rm scholar.bib
+for file in "$@"
+do
+    if [[ -f "$file" ]]; then
+        echo "Using local BibTeX file $file"
+        cp "$file" scholar.bib
+    else
+        echo "Skipping non-existent file $file"
+        continue
+    fi
+    echo "Converting"
+    pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o temp_biblio.txt
+    cat temp_biblio.txt >> biblio.txt
+    rm scholar.bib temp_biblio.txt
+done
